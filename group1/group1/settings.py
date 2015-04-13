d0c8e32535b10b18d2cbbe0970b290ff03b48383
sameran <sameran@bu.edu>
2015-04-13 19:02:12 -0400

    change static and database to outside the git folder for build automation, reorganized static folder per app and in a top-level directory for collectstatic

--- group1/group1/settings.py
+++ group1/group1/settings.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -63,7 +64,7 @@ WSGI_APPLICATION = 'group1.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../../../database/db.sqlite3'),
     }
 }
 
@@ -85,3 +86,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../../../static'))
