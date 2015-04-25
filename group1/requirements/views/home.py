ffaeadda1ccd4d405594153a9ed2f1ed62b7b14b
Stanley Chiang <mraaa@bu.edu>
2015-04-25 14:00:13 -0400

    Bugfix/FixLinksToTeam2and3
    1. Fix links in Side Bar to link to Team2 or Team3

--- group1/requirements/views/home.py
+++ group1/requirements/views/home.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render, redirect
 
 def home_page(request):
-	if request.user.is_authenticated():
-		return redirect('/req/projects')
 	return render(request, 'Home.html')
