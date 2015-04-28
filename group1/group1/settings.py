17b7e9ed0d25dc4467423a7c0e91cb6b93779445
sameran <sameran@bu.edu>
2015-04-27 22:22:19 -0400

    minor change to settings.py

--- group1/group1/settings.py
+++ group1/group1/settings.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = 'group1.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../../database/db.sqlite3'),
     }
 }
 
