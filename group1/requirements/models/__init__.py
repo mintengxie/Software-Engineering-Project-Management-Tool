59ff83829cfe8c43b2aabde3062816863419492e
Abhyudaya Alva <aalva@bu.edu>
2016-04-21 20:27:11 -0400

    add IPaddress to model

--- group1/requirements/models/__init__.py
+++ group1/requirements/models/__init__.py
@@ -7,3 +7,4 @@ from requirements.models.iteration import Iteration
 import requirements.models.iteration
 from requirements.models.files import ProjectFile
 from requirements.models.story_comment import StoryComment
+from requirements.models.ip import IPaddress
